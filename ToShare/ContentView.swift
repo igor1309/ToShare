81513dd4c3c2f865d942ee9a257d84fdd60474c8
Igor Malyarov <photo.igor@mac.com>
2019-07-16 19:23:38 +0300

    Initial Commit

---
+++ ToShare/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ToShare
+//
+//  Created by Igor Malyarov on 16.07.2019.
+//  Copyright © 2019 Igor Malyarov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
